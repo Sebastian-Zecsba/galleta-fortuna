b991d535ffec1e81df59ad9c6b9227e7b1ff518f
Sebastian <sebastiancasallasarias2020@gmail.com>
2024-05-24 18:57:54 -0500

    Proyect set up

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import DisplayText from "./components/DisplayText";
 import ImageRandom from "./components/ImageRandom";
 
+import phrases from './utils/phrases.json'
+
 function App() {
   const images = {
     imageOne: '../../assets/fondo1.png',
@@ -10,11 +12,17 @@ function App() {
     imageFour: '../../assets/fondo4.png'
   };
 
-  const [imageRandom, setImageRandom] = useState('../../assets/fondo1.png');
+  const [imageRandom, setImageRandom] = useState(images.imageOne);
+  const [randomPhrase, setRandomPhrase] = useState(null)
+  console.log(randomPhrase)
+
 
   const handleImageRandom = () => {
     const imagesKeys = Object.keys(images);
     const randomKey = imagesKeys[Math.floor(Math.random() * imagesKeys.length)];
+    const randomIndex = Math.floor(Math.random() * phrases.length)
+    
+    setRandomPhrase(phrases[randomIndex])
     setImageRandom(images[randomKey]);
   };
 
@@ -24,9 +32,20 @@ function App() {
         <ImageRandom imageRandom={imageRandom} />
       </div>
       <div className="box_text">
-        <h1>Galletas de la fortuna</h1>
-        <button onClick={handleImageRandom}>Probar mi suerte</button>
-        <DisplayText />
+        <div className="box_text_main">
+          <h1>Galletas de la fortuna</h1>
+          <button onClick={handleImageRandom}>Probar mi suerte</button>
+          <DisplayText 
+            randomPhrase={randomPhrase}
+          />
+        </div>
+
+        {randomPhrase ?  (
+                <div className="box_autor">
+                    <p className="text_fortune">{randomPhrase.author}</p>
+                </div> )
+                :
+                <p className="text_fortune">Presiona para ver tu suerte</p>}
       </div>
     </div>
   );
--- src/components/DisplayText.jsx
+++ src/components/DisplayText.jsx
@@ -1,12 +1,18 @@
-const DisplayText = () => {
+const DisplayText = ({randomPhrase}) => {
     return (
         <div className="text-box">
             <div className="box_svg">
                 <img src="../../assets/rectangle2.svg" alt="img" />
             </div>
-            <div>
-                <p>Lo que sospechabas es verdad, procura ser amable con ellos, pues esta bondad ayudará a que todo mejore.</p>
-            </div>
+            {randomPhrase ?
+                (<div>
+                    <p className="text_fortune">{randomPhrase.phrase}</p>
+                </div> )
+                :
+                <p className="text_fortune">Presiona para ver tu suerte</p>
+            }
+
+            
       </div>
     );
   }
